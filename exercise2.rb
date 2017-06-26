c9cff0b9e40f59efc2d1f85cf9e6563cf596e4c6
heybrettbell <brett@grassrootsonline.ca>
2017-06-26 13:26:18 -0400

    Exercise #2 complete

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#Problem #1
+cost_of_meal = 55
+good_tip = cost_of_meal * 0.20
+puts "The tip on this meal should be $#{good_tip}."
+
+#Problem #2
+my_age = 42
+puts "I am curently " + my_age.to_s + " years old."
+
+#Problem #3
+math_problem = 45628 * 7839
+puts "The answer to this math problem is #{math_problem}!"
+
+#Problem #4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
